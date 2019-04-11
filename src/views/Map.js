646eb4be525e959c7d297ecd910faeed09068a44
tannerlueders <tlueders@allbound.com>
2019-04-11 15:52:56 -0700

    added base marker and got location

--- src/views/Map.js
+++ src/views/Map.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet'
 
 class MapView extends Component {
-    state = {
-        lat: 51.505,
-        lng: -0.09,
-        zoom: 0,
+    constructor() {
+        super();
+        this.state = {
+            lat: 51.505,
+            lng: -0.09,
+            zoom: 10
+        }
     }
+
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition((position) => {
+            this.setState({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            });
+        });
+    }
+
     render() {
+        const pointerIcon = new L.Icon({
+            iconUrl: require('../assets/marker-icon.png'),
+            iconSize: [25, 41]
+        })
+
         const position = [this.state.lat, this.state.lng]
+
         return(
-            <div className="map">
-                <Map center={position} zoom={this.state.zoom}>
-                    <TileLayer
-                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={position}>
+            <Map className="map" center={position} zoom={this.state.zoom}>
+                <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position} icon={pointerIcon}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
-                    </Marker>
-                </Map>
-            </div>
+                </Marker>
+            </Map>
         );
     }
 }
